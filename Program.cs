2ca071b8a04ed278eb4f4b7a9540d9bdb38e9cb2
Roma <roman.davydchuk.login@gmail.com>
2023-06-25 19:27:06 +0300

    Added the ability to render rotated doughnut

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿const int QuadrantSize = 20;
 const int InnerRadius = 5;
 const int OuterRadius = 10;
+const double RotationAngle = Math.PI / 9;
 
 static void DrawPixel(int x, int y)
 {
@@ -14,11 +15,19 @@ static bool IsInBound(int x, int y)
     return foo * foo <= InnerRadius * InnerRadius;
 }
 
+static bool IsInBoundRotated(int x, int y)
+{
+    var cos = Math.Cos(RotationAngle);
+    var sin = Math.Sin(RotationAngle);
+    var foo = Math.Sqrt(x * x + y * y * cos * cos) - OuterRadius;
+    return foo * foo + y * y * sin * sin <= InnerRadius * InnerRadius;
+}
+
 for (int x = -QuadrantSize; x < 2 * QuadrantSize; x++)
 {
     for (int y = -QuadrantSize; y < 2 * QuadrantSize; y++)
     {
-        if (IsInBound(x, y))
+        if (IsInBoundRotated(x, y))
         {
             DrawPixel(x, y);
         }
